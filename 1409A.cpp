942c6cf76d265039345231e0a1455c898a0ad5bb
Zafor Iqbal <xoy4444@gmail.com>
2023-09-09 09:32:52 +0600

    aa

---
+++ 1409A.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std ;
+int main(){
+    int test,a,b;
+    cin>>test;
+    for (int i=0;i<test;i++){
+        cin>>a>>b;
+        if (a==b){
+            cout<<0<<endl;
+        }
+        else {
+            if (a>b){
+                int temp=a;
+                a=b;
+                b=temp;
+            }
+            while (a<)
+        }
+    }
+
+    return 0;
+}
